b3752ed67f4d3fd0dca7b78074012e7697fc9c53
Sami Saada <sami.saada@gmail.com>
2019-02-18 16:16:51 +0200

    Cache expansions

--- berberbot/main.py
+++ berberbot/main.py
@@ -13,6 +13,10 @@ class BerberBot(sc2.BotAI):
     def on_end(self, game_result):
         pass
 
+    def _prepare_first_step(self):
+        super()._prepare_first_step()
+        self.expansion_locations # cache expansion locations
+
     async def on_step(self, iteration):
         actions = []
         actions += await self.macro.on_step(iteration)
@@ -21,6 +25,7 @@ class BerberBot(sc2.BotAI):
 
         if iteration == 0:
             await self.chat_send(f"GLHF")
+            await self.chat_send(f"All {len(self.expansion_locations)} expansions belong to me!")
 
     async def on_unit_created(self, unit: Unit):
         await self.macro.on_unit_created(unit)
