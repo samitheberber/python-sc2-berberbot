2e2595baf2a95c3e85f2281458c4b6892466a058
Sami Saada <sami.saada@gmail.com>
2019-02-18 16:53:51 +0200

    Move gas worker detection under macro manager and enable unit logging

--- berberbot/macro.py
+++ berberbot/macro.py
@@ -1,3 +1,9 @@
+import logging
+logger = logging.getLogger(__name__)
+
+from sc2.constants import DRONE
+from sc2.unit import Unit
+
 from .manager import Manager
 
 class MacroManager(Manager):
@@ -8,3 +14,12 @@ class MacroManager(Manager):
     async def on_step(self, iteration):
         actions = []
         return actions
+
+    async def on_unit_created(self, unit: Unit):
+        logger.info(f"new unit {unit.tag}")
+        if unit.type_id == DRONE and unit.is_returning:
+            return # Just came out from extractor
+        pass
+
+    async def on_unit_destroyed(self, unit_tag):
+        logger.warning(f"unit died {unit_tag}")
